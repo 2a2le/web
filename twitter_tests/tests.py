09a71956d4b6bea9d26612e978dfc3b0c4f4deb1
Alex Branciog <alex.branciog@gmail.com>
2016-06-03 17:09:15 +0300

    Some more tests

--- twitter_tests/tests.py
+++ twitter_tests/tests.py
@@ -17,6 +17,8 @@ class TestUpdateEndpoint(unittest.TestCase):
 
     def setUp(self):
         self.endpoint = twitter_endpoints.TwitterUpdateEndpoint()
+        # will need this to check that the tweet was actually posted
+        self.search_endpoint = twitter_endpoints.TwitterSearchEndpoint()
 
     def test_0_0_0_status(self):
         """
@@ -48,9 +50,12 @@ class TestUpdateEndpoint(unittest.TestCase):
                 'in_reply_to_status_id_str':A_STATUS_ID,
                 'in_reply_to_user_id':eval(USER_ID),
                 'in_reply_to_user_id_str':USER_ID}
+        search_params = {'q':status}
+        search_json = {'status':json}
         self.assertTrue(self.endpoint.check_request(params=params,
-                                                    status_code=200,
                                                     json=json))
+#        selassertTrue(self.search_endpoint.check_request(params=search_params,
+#                                                           json=search_json))
 
     def test_0_1_1_in_reply_to_status_id(self):
         """
@@ -60,5 +65,58 @@ class TestUpdateEndpoint(unittest.TestCase):
         json = {'in_reply_to_status_id':None,
                 'in_reply_to_status_id_str':None}
         self.assertTrue(self.endpoint.check_request(params=params,
-                                                    status_code=200,
+                                                    json=json))
+
+    def test_0_2_0_lat_long(self):
+        """
+        Tries to post a status specifing the place by lat and long
+        """
+        params = {'lat':45, 'long':45}
+        json = {'place':params.copy()}
+        self.assertTrue(self.endpoint.check_request(params=params,
+                                                    json=json))
+
+
+class TestSearchEndpoint(unittest.TestCase):
+
+    def setUp(self):
+        self.endpoint = twitter_endpoints.TwitterSearchEndpoint()
+
+    def test_1_0_0_search(self):
+        """
+        Tries to post a valid uniq status
+        """
+        params = {'q':'#tweet'}
+        json = {'status':{'text':'#tweet'}}
+        self.assertTrue(self.endpoint.check_request(params=params,
+                                                    json=json))
+
+    def test_1_1_0_search_lang(self):
+        """
+        Tries to post a valid uniq status
+        """
+        params = {'q':'#tweet', 'lang':'sv'}
+        json = {'status':{'text':'#tweet', 'lang':'sv'}}
+        self.assertTrue(self.endpoint.check_request(params=params,
+                                                    json=json))
+
+    def test_1_2_0_search_count(self):
+        """
+        Tries to post a valid uniq status
+        """
+        params = {'q':'#tweet', 'count':2}
+        json = {'status':{'text':'#tweet'}, 'search_metadata':{'count':2},
+                'expected_statuses_number':2}
+        self.assertTrue(self.endpoint.check_request(params=params,
+                                                    json=json))
+
+    def test_1_3_1_search_until(self):
+        """
+        Tries to search for tweets older than a week.
+        Should get no status.
+        """
+        params = {'q':'#tweet', 'until':'2015-01-01'}
+        json = {'search_metadata':{'count':2},
+                'expected_statuses_number':0}
+        self.assertTrue(self.endpoint.check_request(params=params,
                                                     json=json))
--- twitter_tests/twitter_endpoints.py
+++ twitter_tests/twitter_endpoints.py
@@ -1,6 +1,8 @@
 # Alex Branciog
 
 import uuid
+import re
+import time
 
 import requests
 import requests_oauthlib
@@ -15,6 +17,9 @@ CLIENT_KEY = u'eVP3L5l1vX6Q20qEWWJBelIfV'
 CLIENT_SECRET = u'27VEuF4qErAGCishfleG5zLvBJ1AAmJ3IoVdYJ75xQQxxga9UG'
 RESOURCE_OWNER_KEY = u'3575504721-dIpxGbqa4crzDWhwKo16dV56L4lyYHHgi0qczhx'
 RESOURCE_OWNER_SECRET = u'IzalISFtV5JLLz7XszaFz8ajUPPZhaGMIoG86aTAuIjEC'
+
+# Max wait between tweet is posted and tweet is searchable
+SEARCH_TIMEOUT = 30
 
 
 class TestFailedError(AssertionError):
@@ -57,12 +62,29 @@ class TwitterEndpoint():
                                   status_code, self.response.status_code))
         return True
 
-    def _validate_json(self, json):
+
+    def _validate_json(self, json={}):
         """
         Checks the response json.
         """
-        raise NotImplementedError('Each twitter endpoint needs to implement '
-                                  '_validate_json.')
+        def _check_errors(errors):
+            for error in errors:
+                if error not in self.json['errors']:
+                    raise TestFailedError(
+                            'ERRORS: Expected: {}, Actual: {}'.format(
+                                json['errors'], self.json['errors']))
+        if json.has_key('errors'):
+            _check_errors(json.pop('errors'))
+        else:
+            for key in json.keys():
+                expected = json[key]
+                actual = self.json[key] if self.json.has_key(key)\
+                            else 'Not present'
+                if not self._check_value(expected, actual):
+                    raise TestFailedError(
+                            'FIELD {}: Expected: {}, Actual: {}'.format(
+                                key, expected, actual))
+        return True
 
     def check_request(self, params={}, status_code=200, json={}):
         """
@@ -70,10 +92,19 @@ class TwitterEndpoint():
         """
         self._send_request(params)
         print self.response.json()
-        return (self._validate_status_code(status_code) and 
+        return (self._validate_status_code(status_code) and
                 self._validate_json(json))
-        
-    
+
+    def _check_value(self, expected, actual):
+        """
+        Search regex if the expected value is a string.
+        Compare otherwise.
+        """
+        if (type(expected) is str and
+                (type(actual) is str or type(actual) is unicode)):
+            return re.search(expected, actual) is not None
+        return expected == actual
+
 
 class TwitterUpdateEndpoint(TwitterEndpoint):
     """
@@ -88,29 +119,35 @@ class TwitterUpdateEndpoint(TwitterEndpoint):
                                       auth=self.auth)
         self.json = self.response.json()
 
+    def _check_place(self, expected_place):
+        """
+        Check that place (lat,long) is contained by place bounding_box
+        """
+        place = self.json['place']
+        if place is None:
+            raise TestFailedError('FIELD place: Expected: expected_place,'
+                                  ' Actual: Not present')
+        min_long, min_lat = place['bounding_box']['coordinates'][0][0]
+        max_long, max_lat = place['bounding_box']['coordinates'][0][2]
+        exp_lat = expected_place['lat']
+        exp_long = expected_place['long']
+        if exp_lat < min_lat or exp_lat > max_lat:
+            raise TestFailedError('FIELD lat: Expected: Within ({}, {}),'
+                    ' Actual: {}'.format(min_lat, max_lat, exp_lat))
+        if exp_long < min_long or exp_long > max_long:
+            raise TestFailedError('FIELD long: Expected: Within ({}, {}),'
+                    ' Actual: {}'.format(min_long, max_long, exp_long))
+
     def _validate_json(self, json = {}):
-        if json.has_key('errors'):
-            for error in json['errors']:
-                if error not in self.json['errors']:
-                    raise TestFailedError(
-                            'ERRORS: Expected: {}, Actual: {}'.format(
-                                json['errors'], self.json['errors']))
-        else:
-            for key in json.keys():
-                expected = json[key]
-                actual = self.json[key] if self.json.has_key(key)\
-                            else 'Not present'
-                if actual != expected:
-                    raise TestFailedError(
-                            'FIELD {}: Expected: {}, Actual: {}'.format(
-                                key, expected, actual))
-        return True
+        if json.has_key('place'):
+            self._check_place(json.pop('place'))
+        return TwitterEndpoint._validate_json(self, json)
 
     def check_request(self, params={}, status_code=200, json={}):
         if not params.has_key('status'):
             text = generate_uuid()
             # do not modify mutable default function argument
-            params = params.copy() 
+            params = params.copy()
             params['status'] = text
             json['text'] = params['status']
         return TwitterEndpoint.check_request(self, params, status_code, json)
@@ -129,15 +166,66 @@ class TwitterSearchEndpoint(TwitterEndpoint):
                                       auth=self.auth)
         self.json = self.response.json()
 
-"""        
-        
-    url
-    auth
-    params
+    def _check_statuses(self, expected_status):
+        """
+        Verifies that each status has the required fields
+        given by expected_status.
+        Values match the regex/string.
+        """
+        statuses = self.json['statuses']
+        if len(self.json['statuses']) == 0:
+            raise TestFailedError(
+                   'FIELD STATUSES: Expected: At least one status,'
+                   ' Actual: No status')
+        for status in self.json['statuses']:
+            for status_key in expected_status.keys():
+                expected = expected_status[status_key]
+                actual = status[status_key] if\
+                            status.has_key(status_key)\
+                            else 'Not present'
+                if not self._check_value(expected, actual):
+                    raise TestFailedError(
+                           'STATUSES FIELD {}: Expected: {},'
+                           ' Actual: {}'.format(status_key, expected,
+                                                actual.encode('utf-8')))
 
-    send_req
-    parse_response
-for each test json some keys are 
-    common
-    speciffic
-"""
+    def _check_metadata(self, expected_metadata):
+        """
+        Verifies search response json metadata.
+        """
+        metadata = self.json['search_metadata']
+        for key in expected_metadata.keys():
+            expected = expected_metadata[key]
+            actual = metadata[key] if metadata.has_key(key)\
+                        else 'Not present'
+            if not self._check_value(expected, actual):
+                raise TestFailedError(
+                        'FIELD {}: Expected: {}, Actual: {}'.format(
+                                key, expected, actual))
+            self._check_value
+
+    def _validate_json(self, json = {}):
+        if json.has_key('search_metadata'):
+            self._check_metadata(json.pop('search_metadata'))
+        if json.has_key('expected_statuses_number'):
+            # check if we got the expected number of statuses
+            exp_no_statuses = json.pop('expected_statuses_number')
+            no_statuses = len(self.json['statuses'])
+            if no_statuses != exp_no_statuses:
+                raise TestFailedError(
+                    'STATUSES: Count: Expected: {}, Actual: {}'.format(
+                            exp_no_statuses, no_statuses))
+        if json.has_key('status'):
+            self._check_statuses(json.pop('status'))
+        return TwitterEndpoint._validate_json(self, json)
+
+    def check_request(self, params={}, status_code=200, json={}):
+        timeout = SEARCH_TIMEOUT
+        while timeout > 0:
+            try:
+                return TwitterEndpoint.check_request(self, params, status_code, json)
+            except TestFailedError:
+                # Need to wait a while before the tweet is searchable
+                time.sleep(5)
+                timeout-=5
+        return TwitterEndpoint.check_request(self, params, status_code, json)
